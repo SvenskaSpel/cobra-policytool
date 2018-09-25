90e2c811ed61e84db967a98ac9e9de77d0b90767
Magnus Runesson (maoz) <Magnus.Runesson@svenskaspel.se>
2018-09-25 14:13:40 +0200

    Adapt to use setuptool in Azure pipelines and generate junit file.
    Also include hardening of versions for some requirements.

--- setup.py
+++ setup.py
@@ -2,13 +2,35 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
 setup(
     name="cobra-policytool",
-    version="1.0.1",
+    version="1.0.2",
     author="Magnus Runesson",
     author_email="Magnus.Runesson@svenskaspel.se",
     description="Tool for manage Hadoop access using Apache Atlas and Ranger.",
@@ -23,12 +45,14 @@ setup(
     data_files=[],
     packages=find_packages(),
     install_requires=[
-        'requests',
+        'requests>=2.18.4,<2.19',
         'requests-kerberos',
         'click'],
-    tests_require=['nose',
+    tests_require=['pytest',
+                   'nose',
                    'mock'],
-    test_suite='tests.test_tagsync',
+    test_suite='tests',
+    cmdclass={'test': PyTest},
     include_package_data=True,
     classifiers=(
         "Environment :: Console",
