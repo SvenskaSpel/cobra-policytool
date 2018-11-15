bc9c0e0eae2fc98cac80d5087f2c39822f0b5e58
JONSTE01 <44812421+JONSTE01@users.noreply.github.com>
2018-11-15 10:34:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(TestCommand):
 
 setup(
     name="cobra-policytool",
-    version="1.1.3",
+    version="1.1.4",
     author="Magnus Runesson",
     author_email="Magnus.Runesson@svenskaspel.se",
     description="Tool for manage Hadoop access using Apache Atlas and Ranger.",
